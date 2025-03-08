afac859d4a1443d713249877d2a2a0a4d6804bf3
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-03-08 12:26:18 +0530

    Hooks and methods in mongoose with JWT

--- 04_backendProjectStructure/src/models/user.models.js
+++ 04_backendProjectStructure/src/models/user.models.js
@@ -45,6 +45,56 @@ const userSchema = new Schema({
     timestamps: true
 })
 
+/*--------------------------------*/ 
+
+// Mongoose pre-save hook to hash the password before saving the user document
+userSchema.pre("save", async function (next) {
+    // Check if the password field has been modified
+    if (!this.isModified("password")) return next();  // If password is not changed, skip hashing and proceed
+
+    // Hash the new password before saving
+    this.password = await bcrypt.hash(this.password, 10);  // Hash the password
+
+    next();  // Proceed to save the document after hashing
+});
+
+
+userSchema.methods.isPasswordCorrect = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)
+}
+
+/*--------------------------------*/ 
+
+
+// Method to generate an access token for a user       "SYNTAX:- jwt.sign(payload, secret, options)"
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            id: this._id,          // Unique user ID from MongoDB
+            email: this.email,      // User's email for authentication
+            username: this.username,// Username of the user
+            fullname: this.fullname // Full name of the user
+        },
+        process.env.ACCESS_TOKEN_SECRET, // Secret key used to sign the token
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY } // Token expiration time
+    );
+};
+
+
+// Method to generate a refresh token for a user
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            id: this._id  // Unique user ID from MongoDB (used to identify the user)
+        },
+        process.env.REFRESH_TOKEN_SECRET, // Secret key used to sign the refresh token
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY } // Token expiration time (longer duration than access token)
+    );
+};
+
+
+/*--------------------------------*/ 
+
 
 
 
